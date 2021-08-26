04322dcacb59bcf58d0c9f304adacf00d4a39f7e
JennBlunt <jennbluntcodes@gmail.com>
2021-08-26 15:16:09 -0400

    Changed approach to Breed and Name searches. Added pseudo code for script

---
+++ script.js
@@ -0,0 +1,10 @@
+// Breed form:
+
+// 1. loop through breed names to generate list from which to select 
+// 2. return number licenced for selected breed on SVGNumberList
+
+// Name form:
+
+// 1. grab name entered from imput
+// 2. use find() method to search for the name
+// 3. retunr numer licenced with name, or a not found message
